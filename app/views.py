f6d9c06322ee9b4491cf704c3e170886dbcc98d2
LERO <ofentswel@gmail.com>
2017-10-07 11:45:09 +0200

    initializing the app

---
+++ app/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+class Home(View):
+	template_name = 'app/index.html'
+	def get(self, request):
+		return render(request, self.template_name)
